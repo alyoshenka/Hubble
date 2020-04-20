001807797cf59debdc3f8e7760db5cf2e27dc3f7
alexi <alexibmost@gmail.com>
2020-04-20 16:03:08 -0700

    faces

--- display.hpp
+++ display.hpp
@@ -15,18 +15,58 @@
 #include "colors.h"
 #include "faces.h"
 
+#include <iostream>
+
+
+enum mood
+{
+    happy,
+    posessed,
+    soulless,
+    lookEyeDown,
+    lookEyeUp,
+    wink,
+    veryNervouos,
+    nervous,
+    peaceful,
+    sad,
+    vengeful,
+    scared,
+    dying,
+    pissed,
+    amused,
+    gone
+};
+
 class display
 {
+    const std::string facesDir = "/thirdparty/sans/";
 
     std::string timeString;
+    mood currentMood;
+    Texture2D faceTex;
+    Vector2 facePos;
 
 public:
 	display() 
     { 
         timeString = "time";
+        setMood(happy);
     }
 
-	~display() { }
+	~display() { UnloadTexture(faceTex); }
+
+    void setMood(mood newMood)
+    {
+        currentMood = newMood;
+        UnloadTexture(faceTex);
+        /*
+        std::string fileName = facesDir + "sansf" + std::to_string((int)currentMood + 1) + ".png";
+        std::cerr << fileName << std::endl;
+        faceTex = LoadTexture(fileName.c_str());
+        facePos.x = facePos.y = 320;
+        */
+    }
 	
 	void update(float frameTime)
 	{
@@ -49,8 +89,10 @@ public:
 	void draw()
 	{
 		DrawText("HUBBLE", 10, 5, 80, A_PURPLE_3);
-		DrawText(FACE_AT, 320, 320, 160, A_PURPLE_4);
+		// DrawText(FACE_AT, 320, 320, 160, A_PURPLE_4);
 		DrawText(timeString.c_str(), 640, 10, 40, A_GREEN_2);
+
+        DrawTexture(faceTex, facePos.x, facePos.y, WHITE);
 	}
 };
 
