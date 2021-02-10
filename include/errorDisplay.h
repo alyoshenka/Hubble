3acf922351e1b39fe9a242b7a764647a0d5708ae
Alexi <alexibmost@gmail.com>
2021-02-10 14:27:25 -0900

    move files

---
+++ include/errorDisplay.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "config.h"
+#if ON_RPI
+#include "/home/jay/raylib/src/raylib.h"
+#else
+#include "raylib.h"
+#pragma warning(disable : 4996)
+#endif
+
+#include <string>
+using std::string;
+
+class errorDisplay
+{
+    const int dispNum = 14;
+    int errStrCnt;
+    int errStrIdx;
+    string *errStrArr;
+
+public:
+    errorDisplay();
+    ~errorDisplay();
+
+    bool addErrString(string errString);
+    void draw();
+};
